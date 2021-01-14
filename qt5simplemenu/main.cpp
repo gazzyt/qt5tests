ee0dff26ea6171446c4428ac4b499b7107e18e02
Gary Thomas <gazzyt@live.co.uk>
2021-01-14 21:44:25 +0000

    Add qt5simplemenu

---
+++ qt5simplemenu/main.cpp
@@ -0,0 +1,14 @@
+#include "simplemenu.h"
+
+int main(int argc, char*argv[])
+{
+    QApplication app(argc, argv);
+
+    SimpleMenu window;
+
+    window.resize(350, 250);
+    window.setWindowTitle("Simple menu");
+    window.show();
+
+    return app.exec();
+}
---
+++ qt5simplemenu/simplemenu.h
@@ -0,0 +1,13 @@
+#ifndef SIMPLEMENU_H
+#define SIMPLEMENU_H
+
+#include <QMainWindow>
+#include <QApplication>
+
+class SimpleMenu : public QMainWindow
+{
+public:
+    SimpleMenu(QWidget* parent = nullptr);
+};
+
+#endif
